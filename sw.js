08c28d8975f8de9c60b798b0ffeb6a097b3d2fe3
fajar sofyan <fajarsofyan260401@gmail.com>
2026-09-14 23:17:51 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V='ocs-v5';
+const V='ocs-v6';
 const CORE=['./','./index.html','./logo-ocs.png','./manifest.json'];
 self.addEventListener('install',e=>{
  e.waitUntil(caches.open(V).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting()));
